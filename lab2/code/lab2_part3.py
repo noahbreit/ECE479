36febde380f792734db4933dbc2ad31f9c70f597
Noah Breit <nhbreit2@illinois.edu>
2024-03-24 23:25:31 -0500

    finished part2 of lab

--- lab2/code/lab2_part3.py
+++ lab2/code/lab2_part3.py
@@ -10,11 +10,24 @@
 from inception_resnet import InceptionResNetV1Norm
 import numpy as np
 import tensorflow as tf
+import os
 
 # Create a model
 model = InceptionResNetV1Norm()
+model.save_weights("code/weights/inception_keras_weights.h5")  # Has been translated from checkpoint
+
+model = InceptionResNetV1Norm()
+model.load_weights("code/weights/inception_keras_weights.h5")  # Has been translated from checkpoint
 
 # Verify the model and load the weights into the net
 print(model.summary())
+print()
 print(len(model.layers))
-model.load_weights("./weights/inception_keras_weights.h5")  # Has been translated from checkpoint
+
+# begin converting to '.tflite' file
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_resnet = converter.convert()
+
+# write to new file
+with open('quantized_resnet_model.tflite', 'wb') as f:
+    f.write(tflite_resnet)
